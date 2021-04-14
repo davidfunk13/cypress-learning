3d88b39afbceb3a5709fb2fe110d0c91ec618f3a
David Funk <davidfunk@davids-mbp.home>
2021-04-14 13:01:16 -0700

    first code challenge of course completed, find price of requested book.

--- cypress/integration/examples/simple.spec.js
+++ cypress/integration/examples/simple.spec.js
@@ -1,18 +1,30 @@
-describe('Browser actions', () => {
-	it('Should load books website', () => {
-		cy.visit('http://books.toscrape.com/index.html', { timeout: 10000 })
-		cy.url().should('include', 'index.html')
+describe("Browser actions", () => {
+	it("Should load books website", () => {
+		cy.visit("http://books.toscrape.com/index.html", { timeout: 10000 });
+		cy.url().should("include", "index.html");
 	})
-	it('Should click on the Travel category', () => {
+
+	it("Should click on the Travel category", () => {
 		// clicks travel link category on the homepage. Double/right click also an option in the api
-		cy.get('a').contains('Travel').click()
+		cy.get("a").contains("Travel").click();
 		// check contents of h1 after navigation
-		cy.get('h1').contains('Travel')
+		cy.get("h1").contains("Travel");
 	})
 
-	it('should display the correct number of books', () => {
+	it("should display the correct number of books", () => {
 		// querySelectorAll equivalent selector counts items in array returned.
+		cy.get(".product_pod").its("length").should("eq", 11);
+	})
 
-		cy.get('.product_pod').its('length').should('eq', 11)
+	it("Should click on the poetry category", () => {
+		cy.get("a").contains("Poetry").click();
+	})
+
+	it("Should click on Olio book", ()=> {
+		cy.get("a").contains("Olio").click();
+	})
+
+	it("Should have correct price tag.", ()=>{
+		cy.get(".price_color").contains("23.88")
 	})
 })
