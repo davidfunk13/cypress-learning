6b3e836e1aa06eef479446c24d969314db12e043
David Funk <davidfunk@davids-mbp.home>
2021-04-14 12:40:03 -0700

    this section of the course completed, basic testing api

--- cypress/integration/examples/simple.spec.js
+++ cypress/integration/examples/simple.spec.js
@@ -1,9 +1,29 @@
 describe('Browser actions', () => {
 	it('Should load correct url', () => {
-		cy.visit('www.google.com', { timeout: 10000 })
+		cy.visit('https://www.lipsum.com/', { timeout: 10000 })
+
+		cy.on('uncaught:exception', (err, runnable) => {
+			// returning false here prevents Cypress from
+			// failing the test
+			return false
+		})
 	})
+
+	it('Should check correct url', () => {
+		cy.url().should('include', 'lipsum.com')
+	})
+
+    //implicit waiting
     
-    it("Should check correct url", ()=>{
-        cy.url().should("include", "google.com");
-    })
+	it('Should wait 5 seconds', () => {
+		cy.wait(5000)
+	})
+
+	//pausing, for debugging
+	it('Should pause the execution', () => {
+		cy.pause()
+	})
+	it('Should find correct element on the page.', () => {
+		cy.get('h1').should('be.visible')
+	})
 })
